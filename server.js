90b4378706a2deb23da9e8c2caf0ecd47b97f8a0
Glitch (fcc-metric-imperial-convert3r) <none>
2020-06-21 01:58:05 +0000

    🍖🤑 Updated with Glitch

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 'use strict';
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const expect      = require('chai').expect;
+const cors        = require('cors');
+const helmet      = require('helmet'); 
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
 var app = express();
+
+app.use(helmet());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
